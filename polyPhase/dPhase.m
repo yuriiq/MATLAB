ede93e407652f9a7f17fa7ca9dc0ac62e1939c09
yuriiq <yuriiq>
2016-05-10 23:56:41 +0300

    Исправлена ошибка в gradTable.
    Signed-off-by: yuriiq <yuriiq>

--- polyPhase/dPhase.m
+++ polyPhase/dPhase.m
@@ -1,5 +1,4 @@
 function [ dPhase ] = dPhase( phaseA, phaseB )
 % Вычисляет разность фаз и приводит их в диапазон +-П
     dPhase = normPhase (minus(phaseA,phaseB));
-    dPhase = phaseShiftIds( dPhase);
 end
